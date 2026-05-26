8deaa35fd2440ac5a9d38813428e7c12f152f250
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-05-26 15:36:54 -0400

    Mark SpringBootApplication with @EnableAsync

--- src/main/java/com/infybuzz/app/SpringBatchApplication.java
+++ src/main/java/com/infybuzz/app/SpringBatchApplication.java
@@ -3,6 +3,7 @@ package com.infybuzz.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @ComponentScan({"com.infybuzz.config", 
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 	"com.infybuzz.writer",
 	"com.infybuzz.listener",
 	"com.infybuzz.controller"})
+@EnableAsync
 public class SpringBatchApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBatchApplication.class, args);
